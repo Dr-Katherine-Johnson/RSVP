60b0cf35d1388cfcb2ad64c2c5853971863256c5
RomanEmmons <roman.emmons@gmail.com>
2020-01-18 12:10:25 -0800

    implements crud and starts testing

---
+++ sampleData.js
@@ -0,0 +1,32 @@
+module.exports = {
+  eventId: 101,
+  limit: true,
+  setLimit: 5,
+  attendees: [
+    {
+      memberId: 501,
+      name: 'George Washington',
+      avatar: 'http://placecorgi.com/100',
+      thumbnail: 'http://placecorgi.com/50',
+      favorite: true
+    }
+  ],
+  eventOrganizer: [
+    {
+      memberId: 502,
+      name: 'Abe Lincoln',
+      avatar: 'http://placecorgi.com/100',
+      thumbnail: 'http://placecorgi.com/50',
+      favorite: true
+    }
+  ],
+  waitlist: [
+    {
+      memberId: 503,
+      name: 'Sam Jackson',
+      avatar: 'http://placecorgi.com/100',
+      thumbnail: 'http://placecorgi.com/50',
+      favorite: true
+    }
+  ]
+};
