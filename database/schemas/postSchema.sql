37db4ac5ea33d7710f90ea922930e1f2099b82a9
RomanEmmons <roman.emmons@gmail.com>
2020-03-06 01:40:10 -0800

    fixing post schema

--- database/schemas/postSchema.sql
+++ database/schemas/postSchema.sql
@@ -9,6 +9,7 @@ update members set waiting=null where waiting=0;
 update members set attending=null where attending=0;
 update members set organizing=null where organizing=0;
 
--- ALTER TABLE `members` ADD FOREIGN KEY (waiting) REFERENCES `events` (`id`);
--- ALTER TABLE `members` ADD FOREIGN KEY (attending) REFERENCES `events` (`id`);
--- ALTER TABLE `members` ADD FOREIGN KEY (organizing) REFERENCES `events` (`id`);
+ALTER TABLE `members` ADD FOREIGN KEY (waiting) REFERENCES `events` (`id`);
+ALTER TABLE `members` ADD FOREIGN KEY (attending) REFERENCES `events` (`id`);
+
+ALTER TABLE `members` ADD FOREIGN KEY (organizing) REFERENCES `events` (`id`);
